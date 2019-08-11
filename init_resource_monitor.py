67de766fdc59bad8ea55ceaaea02928133d5e130
root <root@node-0.appleseeds.shield-pg0.utah.cloudlab.us>
2019-08-11 11:22:07 -0600

    added script for creating node-exporter, cadvisor, prometheus and grafana

---
+++ init_resource_monitor.py
@@ -0,0 +1,69 @@
+import os
+import sys, socket
+
+def writePrometheusConfigFile(name, yml):
+    f = open("/opt/prometheus/" + name, "w")
+    f.write(yml)
+    f.close()
+
+os.system("docker pull prom/node-exporter")
+os.system("docker pull google/cadvisor")
+os.system("docker pull prom/prometheus")
+os.system("docker pull grafana/grafana")
+
+os.system('docker run -d -p 9100:9100 \
+  -v "/proc:/host/proc:ro" \
+  -v "/sys:/host/sys:ro" \
+  -v "/:/rootfs:ro" \
+  --net="host" \
+  prom/node-exporter')
+
+os.system('docker run \
+    -v "/:/rootfs:ro" \
+    -v "/var/run:/var/run:ro" \
+    -v "/sys:/sys:ro" \
+    -v "/var/lib/docker/:/var/lib/docker:ro" \
+    -v "/dev/disk/:/dev/disk:ro" \
+    -p 8080:8080 \
+    --detach=true \
+    --name=cadvisor \
+    google/cadvisor:latest')
+
+
+os.system("mkdir /opt/prometheus")
+os.system("touch /opt/prometheus/prometheus.yml")
+
+
+prometheus = """
+global:
+  scrape_interval:     60s
+  evaluation_interval: 60s
+ 
+scrape_configs:
+  - job_name: prometheus
+    static_configs:
+      - targets: ['%(ip)s:9090']
+        labels:
+          instance: prometheus
+ 
+  - job_name: linux
+    static_configs:
+      - targets: ['%(ip)s.168.91.132:9100']
+        labels:
+          instance: localhost
+""" % dict(ip=sys.argv[1])
+
+writePrometheusConfigFile("prometheus.yml", prometheus)
+
+os.system('docker run -d -p 9090:9090 \
+  -v "/opt/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml" \
+  prom/prometheus')
+
+os.system("mkdir /opt/grafana-storage")
+os.system("chmod 777 -R /opt/grafana-storage")
+
+os.system('docker run -d \
+  -p 3000:3000 \
+  --name=grafana \
+  -v "/opt/grafana-storage:/var/lib/grafana" \
+  grafana/grafana')
