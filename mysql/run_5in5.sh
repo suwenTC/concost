0eb8000e11531f4e665bb65c6513448cbe4de16d
root <root@node-2.appleseed.shield-pg0.utah.cloudlab.us>
2019-08-12 20:06:57 -0600

    back up

--- mysql/run_5in5.sh
+++ mysql/run_5in5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-stat=$(date +%s)
+start=$(date +%s)
 docke exec db0 ./run_queries.sh 5in5_1 > /dev/null 2&>1 &
 docke exec db1 ./run_queries.sh 5in5_2 > /dev/null 2&>1 &
 docke exec db2 ./run_queries.sh 5in5_3 > /dev/null 2&>1 &
@@ -8,5 +8,5 @@ docke exec db3 ./run_queries.sh 5in5_4 > /dev/null 2&>1 &
 docke exec db4 ./run_queries.sh 5in5_5 > /dev/null 2&>1
 end=$(date +%s)
 touch time_cost/oveall.txt
-time_cost=$(( end - stat ))
+time_cost=$(( end - start ))
 echo $time_cost > time_cost/oveall.txt
