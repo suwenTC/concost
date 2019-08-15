41bbeab9daa7620aa32540e306104a5fbe01d1be
root <root@node-0.appleseed.shield-pg0.utah.cloudlab.us>
2019-08-14 18:44:02 -0600

    added files for testing with hadoop

---
+++ big_data/runexample5.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#modify etc/hosts file to avoid connection error
+cp /etc/hosts /etc/hostsbak
+sed -i 's/::1/#::1/' /etc/hostsbak
+cat /etc/hostsbak > /etc/hosts
+rm -rf /etc/hostsbak
+#run wordcount example
+#/usr/bin/restart_hadoop_spark.sh
+
+service ssh restart
+
+# restart hdfs                                                       
+${HADOOP_HOME}/sbin/start-dfs.sh
+
+# restart yarn                                                               
+${HADOOP_HOME}/sbin/start-yarn.sh
+
+# restart spark                                                               
+${SPARK_HOME}/sbin/start-all.sh
+
+${HADOOP_HOME}/bin/hdfs dfs -ls /HiBench/Wordcount/Input/
+
+${HADOOP_HOME}/bin/hadoop dfsadmin -safemode leave
+
+${HIBENCH_HOME}/bin/workloads/micro/wordcount/hadoop/run.sh &
+${HIBENCH_HOME}/bin/workloads/micro/wordcount/hadoop/run.sh &
+${HIBENCH_HOME}/bin/workloads/micro/wordcount/hadoop/run.sh &
+${HIBENCH_HOME}/bin/workloads/micro/wordcount/hadoop/run.sh &
+${HIBENCH_HOME}/bin/workloads/micro/wordcount/hadoop/run.sh 
