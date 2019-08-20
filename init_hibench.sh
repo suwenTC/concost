1fb21268b9df6cf5618e8be0579b3db4c61353ce
root <root@node-0.appleseed.shield-pg0.utah.cloudlab.us>
2019-08-20 06:45:02 -0600

    fixed hibench script

--- init_hibench.sh
+++ init_hibench.sh
@@ -16,4 +16,4 @@ cp $(pwd)/concost/big_data/run_container5.sh $(pwd)/HiBench/docker/scripts
 cp $(pwd)/concost/big_data/run_xwithgap.py $(pwd)/HiBench/docker/scripts
 cp $(pwd)/concost/big_data/runexample.sh $(pwd)/HiBench/docker/opensource-docker/scripts
 cp $(pwd)/concost/big_data/runexample5.sh $(pwd)/HiBench/docker/opensource-docker/scripts
-sudo bash $(pwd)/HiBench/docker/scripts/build_docker.sh
+sudo bash $(pwd)/HiBench/docker/scripts/build_docker.sh open-source
