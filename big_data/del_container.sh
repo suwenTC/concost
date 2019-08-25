28376a766c7f88b3851d00b83c6a759f55d5add5
root <root@instance-1.c.plenary-agility-231813.internal>
2019-08-25 12:59:34 +0000

    added results for spark svm

--- big_data/del_container.sh
+++ big_data/del_container.sh
@@ -5,3 +5,9 @@ docker rm -f spark_5in5_2
 docker rm -f spark_5in5_3
 docker rm -f spark_5in5_4
 docker rm -f spark_5in5_5
+
+docker rm -f 5in5_spark_0
+docker rm -f 5in5_spark_1
+docker rm -f 5in5_spark_2
+docker rm -f 5in5_spark_3
+docker rm -f 5in5_spark_4
