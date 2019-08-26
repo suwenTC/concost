97fb049fa5d39bc36bb61e564842223c24e22379
root <root@instance-2.c.plenary-agility-231813.internal>
2019-08-26 20:20:43 +0000

    back up

--- mysql/init_dbs.sh
+++ mysql/init_dbs.sh
@@ -5,6 +5,7 @@ while [ $i -lt $1 ]
 do
     docker run --name "db$i" -v /proj/shield-PG0/yarp/:/root/sql/ -v $(pwd)/time_cost:/root/time_cost -e MYSQL_ROOT_PASSWORD=1234 -d suwenbrucegu/mysql:latest
     i=$(( $i + 1 ))
+    sleep 10
 done
 
-docker run --name xin1 -v $(pwd)/time_cost:/root/time_cost -e MYSQL_ROOT_PASSWORD=1234 -d suwenbrucegu/mysql:latest
+#docker run --name xin1 -v $(pwd)/time_cost:/root/time_cost -e MYSQL_ROOT_PASSWORD=1234 -d suwenbrucegu/mysql:latest
