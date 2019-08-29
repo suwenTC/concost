8cd14cf4f0e98dfa4c6cf4ffdc8058aaf67ffda8
root <root@instance-6.c.plenary-agility-231813.internal>
2019-08-29 12:07:05 +0000

    back up

---
+++ big_data/runexample_svm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#modify etc/hosts file to avoid connection error
+cp /etc/hosts /etc/hostsbak
+sed -i 's/::1/#::1/' /etc/hostsbak
+cat /etc/hostsbak > /etc/hosts
+rm -rf /etc/hostsbak
+#run wordcount example
+/usr/bin/restart_hadoop_spark.sh
+touch "/root/time_cost/$1.txt"
+start=$(date +%s)
+start_date=$(date)
+${HIBENCH_HOME}/bin/workloads/ml/svm/prepare/prepare.sh
+${HIBENCH_HOME}/bin/workloads/ml/svm/spark/run.sh
+end_date=$(date)
+end=$(date +%s)
+time_cost=$(( end - start ))
+echo "$start_date, $end_date, $time_cost" > "/root/time_cost/spark_$1.txt"
